dfb41f59e8588cddb17d2229a39e52c279722447
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-12 14:10:18 -0400

    doubling letter e and u since whales pronounce those longer than usual

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ for (let i = 0; i < input.length; i++) {
     resultArray.push(input[i]);
   }
 
+  if (input[i] === 'u') {
+    resultArray.push(input[i]);
+  }
+
   for (let j = 0; j < vowels.length; j++) {
     if (input[i] === vowels[j]) {
       resultArray.push(input[i]);
