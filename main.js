fb3e57a49856f40b9ea62eea5a4ac83f51d560db
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-12 14:02:28 -0400

    created a nested loop

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const input = 'Whats up doc?';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 
-for (let i = 0; i < vowels.length; i++){
-  console.log(vowels[i])
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+
+  }
 }
