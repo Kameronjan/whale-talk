ef707cd0581d35f7691e2252547fc02d367c51b0
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-12 14:06:51 -0400

    pushing any vowels in my input to my result array, tested code, good to continue

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ let resultArray = [];
 for (let i = 0; i < input.length; i++) {
   for (let j = 0; j < vowels.length; j++) {
     if (input[i] === vowels[j]) {
-
+      resultArray.push(input[i]);
     }
   }
 }
+
+console.log(resultArray)
